eeddb5ddd0cc61b44fa9d01ef9168376039da99d
Felix Haba <felixh@felixs-mbp.ads.northwestern.edu>
2023-04-16 11:58:08 -0500

    finish up study plan

--- src/database/mockStudyPlanData.ts
+++ src/database/mockStudyPlanData.ts
@@ -3,13 +3,13 @@ export const studyPlan = [
     subject: "Math",
     learningGoal: "Calculus fundamentals",
     studyTime: "Monday, 3pm - 5pm",
-    deadline: "2023-05-01",
+    deadline: "2023-04-20",
   },
   {
     subject: "History",
     learningGoal: "World War II",
     studyTime: "Wednesday, 2pm - 4pm",
-    deadline: "2023-05-15",
+    deadline: "2023-04-17",
   },
   // Add more study plan items here
 ]
--- src/pages/study-plan/index.tsx
+++ src/pages/study-plan/index.tsx
@@ -1,14 +1,16 @@
 // pages/study-plan.tsx
-import { Table, Calendar } from "antd"
+import { Table, Calendar, Modal, List } from "antd"
 import { ColumnsType } from "antd/es/table"
 import moment from "moment"
 import { studyPlan } from "../../database/mockStudyPlanData"
+import { useState } from "react"
 
 type StudyPlanItem = {
   subject: string
   learningGoal: string
   studyTime: string
   deadline: string
+  guidelines: string
 }
 
 const columns: ColumnsType<StudyPlanItem> = [
@@ -43,9 +45,42 @@ const columns: ColumnsType<StudyPlanItem> = [
 ]
 
 const StudyPlanPage = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false)
+  const [selectedDeadline, setSelectedDeadline] =
+    useState<StudyPlanItem | null>(null)
+
+  const showModal = (item: StudyPlanItem) => {
+    setSelectedDeadline(item)
+    setIsModalVisible(true)
+  }
+
+  const handleOk = () => {
+    setIsModalVisible(false)
+  }
+
+  const handleCancel = () => {
+    setIsModalVisible(false)
+  }
   return (
     <div>
       <h1>Study Plan</h1>
+      <h2>Today</h2>
+      <List
+        dataSource={[
+          "Look at chapter 3, page 40, in the history textbook to learn more about the Spanish battle against the British.",
+          "Look at minute 32 of Lecture 10 to learn more about clustered embeddings.",
+        ]}
+        renderItem={(item) => <List.Item>{item}</List.Item>}
+        style={{
+          backgroundColor: "#f0f2f5",
+          borderRadius: "6px",
+          padding: "12px",
+        }}
+      />
+      {/* <ul>
+        <li>Look at chapter 3, page 40, in the history textbook to learn more about the Spanish battle against the British.</li>
+        <li>Look at minute 32 of Lecture 10 to learn more about clustered embeddings.</li>
+      </ul> */}
       <Table<StudyPlanItem>
         dataSource={studyPlan}
         columns={columns}
@@ -68,7 +103,25 @@ const StudyPlanPage = () => {
             </ul>
           )
         }}
+        onSelect={(value) => {
+          const deadline = studyPlan.find((item) =>
+            moment(item.deadline).isSame(value, "day")
+          )
+          if (deadline) {
+            showModal(deadline)
+          }
+        }}
       />
+      {selectedDeadline && (
+        <Modal
+          title={`${selectedDeadline.subject}: ${selectedDeadline.learningGoal}`}
+          visible={isModalVisible}
+          onOk={handleOk}
+          onCancel={handleCancel}
+        >
+          <p>{selectedDeadline.guidelines}</p>
+        </Modal>
+      )}
     </div>
   )
 }
